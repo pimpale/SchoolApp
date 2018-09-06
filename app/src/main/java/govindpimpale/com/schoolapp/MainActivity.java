fa859138fb5c127b048c2d99de16e2f732239b34
pimpale <gpimpale29@gmail.com>
2018-09-05 20:18:51 -0700

    first commit

---
+++ app/src/main/java/govindpimpale/com/schoolapp/MainActivity.java
@@ -0,0 +1,26 @@
+package govindpimpale.com.schoolapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return false;
+            }
+        });
+        webView.loadUrl("file:///android_asset/website/index.html");
+    }
+}
